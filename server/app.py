878f4479f5e74ec77f1566b949cfc766e5be4cac
Mark Coats <alexco1234@gmail.com>
2023-04-12 10:31:53 -0500

    pretty sure it's done, didn't touch front end and all requests seem to work...

--- server/app.py
+++ server/app.py
@@ -20,9 +20,87 @@ def index():
     return '<h1>Code challenge</h1>'
 
 @app.route('/research')
-def restaurants():
+def all_research():
+    ret = []
+    for research in Research.query.all():
+        new = {
+            'id':research.id,
+            'topic': research.topic,
+            'year': research.year,
+            'page_count': research.page_count
+        }
+        ret.append(new)
+    response = make_response(
+        ret,
+        200
+    )
+    return response
 
-    pass
+@app.route('/research/<int:id>', methods=['GET', 'DELETE'])
+def res_by_id(id):
+    ret = Research.query.filter(Research.id == id).first()
+    if ret == None:
+        body = {
+            "error": "Research paper not found"
+        }
+        response = make_response(
+            body,
+            404
+        )
+        return response
+    else:
+        if request.method == 'GET':
+            response = make_response(
+                ret.to_dict(),
+                200
+            )
+            return response
+        elif request.method == 'DELETE':
+            db.session.delete(ret)
+            db.session.commit()
+            body = {
+                "message": 'Delete Successful!'
+            }
+            response = make_response(
+                body, 
+                200
+            )
+            return response
+        
+@app.route('/authors')
+def authors():
+    auth_list = []
+    for auth in Author.query.all():
+        auth_list.append(auth.to_dict())
+    resp = make_response(
+        auth_list,
+        200
+    )
+    return resp
+
+@app.route('/research_author', methods=['GET', 'POST'])
+def research_authors():
+    if request.method == 'GET':
+        auth_list = []
+        for res_auth in ResearchAuthors.query.all():
+            auth_list.append(res_auth.to_dict())
+        resp = make_response(
+            auth_list,
+            200
+        )
+        return resp
+    if request.method == 'POST':
+        new_ra = ResearchAuthors(
+            author_id = request.form.get('author_id'),
+            research_id = request.form.get('research_id')
+        )
+        db.session.add(new_ra)
+        db.session.commit()
+        resp = make_response(
+            new_ra.to_dict(),
+            201
+        )
+        return resp
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -4,10 +4,59 @@ from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+metadata = MetaData(naming_convention = {
+  "ix": "ix_%(column_0_label)s",
+  "uq": "uq_%(table_name)s_%(column_0_name)s",
+  "ck": "ck_%(table_name)s_%(constraint_name)s",
+  "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+  "pk": "pk_%(table_name)s"
 })
 
 db = SQLAlchemy(metadata=metadata)
 
-# Add models here
+class Research(db.Model, SerializerMixin):
+    __tablename__ = 'research'
+    serialize_rules = ('-research_authors.research_id','-research_authors.research')
+
+    id = db.Column(db.Integer, primary_key=True)
+    topic = db.Column(db.String)
+    year = db.Column(db.Integer)
+    page_count = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    research_authors = db.relationship('ResearchAuthors', back_populates='research')
+
+    @validates('year')
+    def validate_year(self, key, year):
+        assert len(year) == 4, 'Year Must Have 4 Digits!'
+        return year
+
+class ResearchAuthors(db.Model, SerializerMixin):
+    __tablename__ = 'research_authors'
+    serialize_rules = ('-research.research_authors', '-authors.research_authors')
+
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
+    research_id = db.Column(db.Integer, db.ForeignKey('research.id'))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    author = db.relationship('Author', back_populates='research_authors')
+    research = db.relationship('Research', back_populates='research_authors')
+
+
+class Author(db.Model, SerializerMixin):
+    __tablename__ = 'authors'
+    serialize_rules = ('-research_authors.author_id', '-research_authors.author')
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    field_of_study = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    research_authors = db.relationship('ResearchAuthors', back_populates='author')
+
+    @validates('field_of_study')
+    def validate_fos(self, key, field_of_study):
+        possible = ['AI', 'Robotics', 'Machine Learning', 'Vision', 'Cybersecurity']
+        assert field_of_study in possible, f"Field of study must be one of {possible}"
+        return field_of_study
